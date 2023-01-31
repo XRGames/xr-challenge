49903b6065d61a9863f89776bee219a2e6f3562a
I-Am-Extra <63207577+I-Am-Extra@users.noreply.github.com>
2023-01-31 19:01:19 +0000

    Organised File Structure + Asm-defs

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Associate this script with the XR.Player library
+//On compile Unity will make a .dll for each assembly definition, allowing us to organize our code into libraries to promote modularity and reusability
+//By default every script without a .asmdef will be put into Assembly-CSharp.dll
+namespace XR.Player
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }   
+
+}
