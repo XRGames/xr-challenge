f6307bab7cfac44215ba18ddc05927054eac298b
c017176m <114945574+c017176m@users.noreply.github.com>
2023-12-28 20:40:58 +0000

    Prog: Event System & Input Asset
    - Figured better structure of inputs for future expanding and simplicity

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,12 +13,6 @@ public class EventManager : MonoBehaviour
 	[SerializeField]
 	private PlayerInput PlayerInput;
 
-
-	//void Start()
-	//{
-	//	BasicEvent();
-	//}
-
 	private void Awake()
 	{
 		PlayerInput = GetComponent<PlayerInput>();
@@ -25,35 +20,104 @@ public class EventManager : MonoBehaviour
 
 	private void OnEnable()
 	{
-		PlayerInput.currentActionMap.FindAction("Action").started += Subscrip_Func;
+		//Reminds what input map you have selected in editor
+		PlayerInput.SwitchCurrentActionMap("RH_Movement");
+		Debug.Log("Current Action Map is: " + PlayerInput.currentActionMap.name);
 
-		PlayerInput.currentActionMap["Walk"].performed += Subscrip_Func;
-		PlayerInput.currentActionMap["Walk"].canceled += Subscrip_Func;
+		/*
+		 * These subscribe (successful inputs understood inside the input 
+		 * asset) to (an event which broadcasts info about the occured input)
+		 */
+		PlayerInput.currentActionMap["Walk"].performed += Subscrip_Test_Func;
+		PlayerInput.currentActionMap["Walk"].canceled += Subscrip_Test_Func;
+
+		PlayerInput.currentActionMap["Sprint"].performed += Subscrip_Test_Func;
+		PlayerInput.currentActionMap["Sprint"].canceled += Subscrip_Test_Func;
+
+		PlayerInput.currentActionMap["Jump"].performed += Subscrip_Test_Func;
+		PlayerInput.currentActionMap["Jump"].canceled += Subscrip_Test_Func;
+
 	}
 
 	private void OnDisable()
 	{
-		PlayerInput.currentActionMap.FindAction("Action").started -= Subscrip_Func;
+		/*
+		 * These unsubscribe the existing inputs defined above. Unsubscribing is
+		 * not automatic, therefore needs manual clean-up when the gameObject 
+		 * is disabled
+		 */
+		if (PlayerInput.currentActionMap.actions.Count > 0)
+		{
+			PlayerInput.currentActionMap["Walk"].performed -= Subscrip_Test_Func;
+			PlayerInput.currentActionMap["Walk"].canceled -= Subscrip_Test_Func;
 
-		PlayerInput.currentActionMap["Walk"].performed -= Subscrip_Func;
-		PlayerInput.currentActionMap["Walk"].canceled -= Subscrip_Func;
+			PlayerInput.currentActionMap["Sprint"].performed -= Subscrip_Test_Func;
+			PlayerInput.currentActionMap["Sprint"].canceled -= Subscrip_Test_Func;
+
+			PlayerInput.currentActionMap["Jump"].performed -= Subscrip_Test_Func;
+			PlayerInput.currentActionMap["Jump"].canceled -= Subscrip_Test_Func;
+		}
 	}
 
 	private void OnApplicationQuit()
 	{
-		if (PlayerInput.currentActionMap.actions.Count > 0)
-		{
-			PlayerInput.currentActionMap.FindAction("Action").started -= Subscrip_Func;
+		/*
+		 * A last resort which kicks in if the movement events don't 
+		 * unsubscribe properly
+		 */
+		PlayerInput.currentActionMap["Walk"].performed -= Subscrip_Test_Func;
+		PlayerInput.currentActionMap["Walk"].canceled -= Subscrip_Test_Func;
 
-			PlayerInput.currentActionMap["Walk"].performed -= Subscrip_Func;
-			PlayerInput.currentActionMap["Walk"].canceled -= Subscrip_Func;
+		PlayerInput.currentActionMap["Sprint"].performed -= Subscrip_Test_Func;
+		PlayerInput.currentActionMap["Sprint"].canceled -= Subscrip_Test_Func;
 
-			Debug.LogWarning("Input Actions failed to un-subscribe on " + this.gameObject + ". They have been un-subscribed using OnApplicationQuit()");
-		}
+		PlayerInput.currentActionMap["Jump"].performed -= Subscrip_Test_Func;
+		PlayerInput.currentActionMap["Jump"].canceled -= Subscrip_Test_Func;
+
+		Debug.LogWarning("Input Actions failed to un-subscribe on " + this.gameObject + ". They have been un-subscribed using OnApplicationQuit()");
+
 	}
 
-	private void Subscrip_Func(InputAction.CallbackContext actionContext)
+	//private void Subscrip_Test_Func3(InputAction.CallbackContext actionContext)
+	//{
+	//	BasicEvent("Input info: " + actionContext.ReadValue<Vector2>());
+
+	//}
+
+	//private void Subscrip_Test_Func2(InputAction.CallbackContext actionContext)
+	//{
+	//	BasicEvent("Input info: " + actionContext.ReadValue<float>());
+	//}
+
+	private void Subscrip_Test_Func(InputAction.CallbackContext actionContext)
 	{
-		BasicEvent("Input info: " + actionContext.ReadValue<Vector2>());
+		//if (actionContext.action.name == "Walk")
+		//{
+		//	BasicEvent( "Input info: " + actionContext.action.name + " " + actionContext.ReadValue<Vector2>());
+		//}
+		//else if (actionContext.action.name == "Sprint")
+		//{
+		//	BasicEvent("Input info: " + actionContext.action.name + " " + actionContext.ReadValue<float>());
+		//}
+		//else if (actionContext.action.name == "Jump")
+		//{
+		//	BasicEvent("Input info: " + actionContext.action.name + " " + actionContext.ReadValue<float>());
+		//}
+
+		switch(actionContext.action.name)
+		{
+			case "Jump":
+				BasicEvent("Input info: " + actionContext.action.name + " " + actionContext.ReadValue<float>());
+				break;
+			case "Sprint":
+				BasicEvent("Input info: " + actionContext.action.name + " " + actionContext.ReadValue<float>());
+				break;
+			case "Walk":
+				BasicEvent("Input info: " + actionContext.action.name + " " + actionContext.ReadValue<Vector2>());
+				break;
+			default:
+				Debug.Log("No valid input type");
+				break;
+		}
 	}
 }
