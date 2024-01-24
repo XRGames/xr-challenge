d1c21f3a9d3ffba90d24727694f0105319114cf3
Daniel Henshaw <danielhenshaw03@gmail.com>
2024-01-24 19:43:24 +0000

    Added a pickup handler
    Interacts with the prebuilt pickup system allowing the player to pickup the star and increment the score GUI.

---
+++ Assets/_Project/Code/Scripts/Pickup/PickupHandler.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+public class PickupHandler : MonoBehaviour
+{
+  [Header("References")]
+  [SerializeField] private TextMeshProUGUI scoreText;
+
+  private int score = 0;
+  public int Score => score;
+
+  private void Update()
+  {
+    scoreText.text = "SCORE: " + score;
+  }
+
+  private void OnTriggerEnter(Collider c)
+  {
+    if (c.CompareTag("Pickup"))
+    {
+      score += c.GetComponent<Pickup>().GetPickedUp();
+    }
+  }
+}
